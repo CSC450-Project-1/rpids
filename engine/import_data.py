7710b1847b00067ead5c311f7c15af289a2f57fb
Austin <austinpearce15@yahoo.com>
2021-02-10 21:57:39 -0600

    Clean up import process

--- engine/import_data.py
+++ engine/import_data.py
@@ -12,30 +12,26 @@ import pandas as pd
 # Get label information
 def read_label():
     column = []
-    label_file = "Metal Detection Sensor Array/Data_Label.csv" # TODO: for testing
+    label_file = "sample_data/Data_Label.csv" # TODO: for testing
 
-    with open(label_file) as label_csv:
-        csvReader = csv.reader(label_csv)
-        for row in csvReader:
+    with open(label_file) as file:
+        reader = csv.reader(file)
+        for row in reader:
             column = row
         if len(column) == 0:
             return print("Invalid label file!")
 
         return column
+
 def read_data():
-        df = pd.read_csv("Metal Detection Sensor Array/Measurement1.csv") # TODO: for testing
+        df = pd.read_csv("sample_data/Measurement1.csv") # TODO: for testing
         return df
         
 def main(): 
     label = read_label()
     data = read_data()
     data.columns  = [label]
-    # df = pd.DataFrame(data, columns = label)
     print(data)
-    # TODO: testing
-    # df = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), columns=['a', 'b', 'c'])
-    # print(df)
-    # print(df.get('a'))
 
 if __name__ == "__main__":
     main()
