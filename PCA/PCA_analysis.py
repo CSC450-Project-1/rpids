16fd13ae06f1e129c886da2e30099165a72ffb53
Rich Russell <Russell604@live.missouristate.edu>
2021-02-14 15:41:12 -0600

    adding sample data

--- PCA/PCA_analysis.py
+++ PCA/PCA_analysis.py
@@ -1,2 +1,15 @@
 import numpy as np
+
 import pandas as pd
+
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import accuracy_score
+
+from matplotlib.colors import ListedColormap
+
+
+
+dataset = pd.read_csv(r'dataset)
+
+dataset.head()
