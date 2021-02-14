b4bb12e6440348fd0b256d8b0c197fc597a6dfbe
Rich Russell <Russell604@live.missouristate.edu>
2021-02-14 16:47:14 -0600

    trouble shooting

--- PCA/PCA_analysis.py
+++ PCA/PCA_analysis.py
@@ -1,17 +1,11 @@
 import numpy as np
-
 import pandas as pd
-
 import matplotlib.pyplot as plt
-
 from sklearn.metrics import accuracy_score
-
 from matplotlib.colors import ListedColormap
 
 
-
-dataset = pd.read_csv(r'dataset)
-
+dataset = pd.read_csv(r'dataset')
 dataset.head()
 
 #split into dependant and independent variable
